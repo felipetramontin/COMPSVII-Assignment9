f163f865829ff0d39571551d674e22c68219ec57
Felipe Tramontin <felipetramontin2008@gmail.com>
2025-10-27 20:30:32 -0400

    extra credit done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 fetchGifButton = document.getElementById("fetch-gif-btn");
 gifContainer = document.getElementById("gif-container");
-
-endpoint = "https://api.giphy.com/v1/gifs/search?api_key=SrAdsK2thLzccU5oTUbR5tHXluhAgM1x&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+searchInput = document.getElementById("search-input");
 
 async function fetchGifs() {
+    gifContainer.innerHTML = "";
+    search = searchInput.value.trim();
+    endpoint = `https://api.giphy.com/v1/gifs/search?api_key=SrAdsK2thLzccU5oTUbR5tHXluhAgM1x&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
     try {
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        const images = data.data;
+        response = await fetch(endpoint);
+        data = await response.json();
+        images = data.data;
         images.forEach(gif => {
-            const imageUrl = gif.images.fixed_height.url;
+            imageUrl = gif.images.fixed_height.url;
             gifContainer.innerHTML += `<img src=${imageUrl} class="col-3 mb-3">`;
         });
     } catch (error) {
